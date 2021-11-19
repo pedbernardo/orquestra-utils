ea43985662d145747d8adae0eb6ae39979edf101
Pedro Bernardo <pedrovbernardo@gmail.com>
2021-11-19 20:08:44 -0300

    chore: update husky, build and rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,13 @@ export default {
   input: './src/index.js',
   output: [
     {
+      file: 'dist/orquestra-utils.esm.js',
+      format: 'esm'
+    },
+    {
       name: 'Utils',
       file: 'dist/orquestra-utils.js',
       format: 'umd'
-    },
-    {
-      file: 'dist/orquestra-utils.esm.js',
-      format: 'esm'
     },
     {
       name: 'Utils',
