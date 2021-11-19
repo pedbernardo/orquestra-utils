c25d112aa352587f73f65e412fdda7b3ed1d23db
Pedro Bernardo <pedrovbernardo@gmail.com>
2021-11-19 20:54:04 -0300

    chore: set output by package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
+import pkg from './package.json'
+
 export default {
   input: './src/index.js',
   output: [
     {
-      file: 'dist/orquestra-utils.esm.js',
+      file: pkg.module,
       format: 'esm'
     },
     {
       name: 'Utils',
-      file: 'dist/orquestra-utils.js',
+      file: pkg.browser,
       format: 'umd'
     }
   ]
